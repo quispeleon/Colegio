f2affbf47d6a1bcb04232e87ca9588a334855c2b
ZupraReal <quispeleonmariabrenda@gmail.com>
2025-06-25 14:35:12 -0300

    si

--- Biblioteca/obj/Debug/net9.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net9.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca38a0056106f05ec181cd05a07f36c212fedfd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
